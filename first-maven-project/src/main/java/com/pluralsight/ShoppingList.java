60a565c77ad690ba978048dd0e949029a5b8d917
Brenda Vang <vangsbrenda@gmail.com>
2025-04-10 13:33:16 -0500

    completed ShoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Chicken");
+        System.out.println("Ice Cream");
+        System.out.println("Bacon");
+        System.out.println("Strawberries");
+        System.out.println("Bananas");
+        System.out.println("Bread");
+        System.out.println("Pasta");
+        System.out.println("Waffles");
+        System.out.println("Juice");
+        System.out.println("Kombucha");
+        
+    }
+}
